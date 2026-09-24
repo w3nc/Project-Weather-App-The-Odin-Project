15c1bbc3ef733b23397ee2061b75c883294a27a8
w3nc <199257164+w3nc@users.noreply.github.com>
2026-09-24 17:42:30 +0800

    feat(utils): escape HTML for user-controlled strings

---
+++ src/utils/escape.js
@@ -0,0 +1,13 @@
+export function escapeHtml(str) {
+  return String(str).replace(
+    /[&<>"']/g,
+    (c) =>
+      ({
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': "&quot;",
+        "'": "&#39;",
+      })[c],
+  );
+}
